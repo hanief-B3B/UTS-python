6ec9a2989ae2c9da502bed50fd5d2f09897f5400
Hanief Bintang Tri Buana <haniefbintang3@gmail.com>
2025-04-25 23:35:32 +0700

    Update jawaban-no-7.py

--- jawaban-no-7.py
+++ jawaban-no-7.py
@@ -1,9 +1,15 @@
 def kategori_usia(usia):
     if usia < 12:
         return "Anak-anak"
-    elif 12 <= usia <= 17:
+    elif usia < 18:
         return "Remaja"
-    elif 18 <= usia <= 59:
+    elif usia < 60:
         return "Dewasa"
     else:
         return "Lansia"
+
+# Contoh penggunaan
+print(kategori_usia(10))  # Output: Anak-anak
+print(kategori_usia(15))  # Output: Remaja
+print(kategori_usia(30))  # Output: Dewasa
+print(kategori_usia(65))  # Output: Lansia
